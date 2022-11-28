b3fbfe3f30d48b63e06e01f54cb979dad6a42650
Bárbara Ulloa <ulloaby@gmail.com>
2022-11-28 02:52:57 -0300

    create component 'HorizontalBarChart'

---
+++ src/components/HorizontalBarChart.vue
@@ -0,0 +1,51 @@
+<template>
+  <Bar :chart-data="chartData" :chart-options="chartOptions" />
+</template>
+<script setup>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+const props = defineProps({
+  labels: {
+    type: Array,
+    required: true,
+  },
+  datasets: {
+    type: Array,
+    required: true,
+  },
+});
+
+const chartData = {
+  labels: props.labels,
+  datasets: props.datasets,
+  backgroundColor: "blue",
+};
+
+const chartOptions = {
+  responsive: true,
+  indexAxis: "y",
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+</script>
