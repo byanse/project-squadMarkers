6137993abcafa5c3676feba7f7c0e178a48063f9
Bárbara Ulloa <ulloaby@gmail.com>
2022-11-28 02:48:42 -0300

    create component 'BtnBankButton' to handle back navigation

---
+++ src/components/BtnBackButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-btn flat round dense :icon="icon" color="neutral-black" @click="goBack" />
+</template>
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+defineProps({
+  icon: {
+    type: String,
+    default: "arrow_back",
+  },
+});
+
+const goBack = () => {
+  router.back();
+};
+</script>
